ac6f0f063216d452e998d22634a92b97e7fe278a
obones <obones@free.fr>
2015-12-30 15:37:48 +0100

    Rows/Columns configuration

--- keyboard/presario_2710/matrix.c
+++ keyboard/presario_2710/matrix.c
@@ -135,77 +135,88 @@ uint8_t matrix_key_count(void)
 }
 
 /* Column pin configuration
- * col: 0   1   2   3   4   5   6   7   8   9   10  11  12  13
- * pin: F0  F1  E6  C7  C6  B6  D4  B1  B0  B5  B4  D7  D6  B3  (Rev.A)
- * pin:                                 B7                      (Rev.B)
+ * col: 0   1   2   3   4   5   6   7   8   9   10  11  12  13  14  15
+ * pin: F0  F1  F2  F3  F4  F5  F6  F7  B0  B1  B2  B3  B4  B5  B6  E7
  */
 static void  init_cols(void)
 {
     // Input with pull-up(DDR:0, PORT:1)
-    DDRF  &= ~(1<<0 | 1<<1);
-    PORTF |=  (1<<0 | 1<<1);
-    DDRE  &= ~(1<<6);
-    PORTE |=  (1<<6);
-    DDRD  &= ~(1<<7 | 1<<6 | 1<<4);
-    PORTD |=  (1<<7 | 1<<6 | 1<<4);
-    DDRC  &= ~(1<<7 | 1<<6);
-    PORTC |=  (1<<7 | 1<<6);
-    DDRB  &= ~(1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
-    PORTB |=  (1<<7 | 1<<6 | 1<< 5 | 1<<4 | 1<<3 | 1<<1 | 1<<0);
+    DDRF  &= ~(0b11111111);
+    PORTF |=  (0b11111111);
+    DDRE  &= ~(1<<7);
+    PORTE |=  (1<<7);
+    DDRB  &= ~(0b01111111);
+    PORTB |=  (0b01111111);
 }
 
 static matrix_row_t read_cols(void)
 {
     return (PINF&(1<<0) ? 0 : (1<<0)) |
            (PINF&(1<<1) ? 0 : (1<<1)) |
-           (PINE&(1<<6) ? 0 : (1<<2)) |
-           (PINC&(1<<7) ? 0 : (1<<3)) |
-           (PINC&(1<<6) ? 0 : (1<<4)) |
-           (PINB&(1<<6) ? 0 : (1<<5)) |
-           (PIND&(1<<4) ? 0 : (1<<6)) |
-           (PINB&(1<<1) ? 0 : (1<<7)) |
-           ((PINB&(1<<0) && PINB&(1<<7)) ? 0 : (1<<8)) |     // Rev.A and B
-           (PINB&(1<<5) ? 0 : (1<<9)) |
-           (PINB&(1<<4) ? 0 : (1<<10)) |
-           (PIND&(1<<7) ? 0 : (1<<11)) |
-           (PIND&(1<<6) ? 0 : (1<<12)) |
-           (PINB&(1<<3) ? 0 : (1<<13));
+           (PINF&(1<<2) ? 0 : (1<<2)) |
+           (PINF&(1<<3) ? 0 : (1<<3)) |
+           (PINF&(1<<4) ? 0 : (1<<4)) |
+           (PINF&(1<<5) ? 0 : (1<<5)) |
+           (PINF&(1<<6) ? 0 : (1<<6)) |
+           (PINF&(1<<7) ? 0 : (1<<7)) |
+           (PINB&(1<<0) ? 0 : (1<<8)) | 
+           (PINB&(1<<1) ? 0 : (1<<9)) |
+           (PINB&(1<<2) ? 0 : (1<<10)) |
+           (PINB&(1<<3) ? 0 : (1<<11)) |
+           (PINB&(1<<4) ? 0 : (1<<12)) |
+           (PINB&(1<<5) ? 0 : (1<<13)) |
+           (PINB&(1<<6) ? 0 : (1<<14)) |
+           (PINE&(1<<7) ? 0 : (1<<15));
 }
 
 /* Row pin configuration
- * row: 0   1   2   3   4
- * pin: D0  D1  D2  D3  D5
+ * row: 0   1   2   3   4   5   6   7
+ * pin: C0  C1  C2  C3  C4  C5  C6  C7
  */
 static void unselect_rows(void)
 {
     // Hi-Z(DDR:0, PORT:0) to unselect
-    DDRD  &= ~0b00101111;
-    PORTD &= ~0b00101111;
+    DDRC  &= ~0b11111111;
+    PORTC &= ~0b11111111;
 }
 
 static void select_row(uint8_t row)
 {
     // Output low(DDR:1, PORT:0) to select
-    switch (row) {
+    DDRC  |= (1<<row);
+    PORTC &= ~(1<<row);
+/*    switch (row) {
         case 0:
-            DDRD  |= (1<<0);
-            PORTD &= ~(1<<0);
+            DDRC  |= (1<<0);
+            PORTC &= ~(1<<0);
             break;
         case 1:
-            DDRD  |= (1<<1);
-            PORTD &= ~(1<<1);
+            DDRC  |= (1<<1);
+            PORTC &= ~(1<<1);
             break;
         case 2:
-            DDRD  |= (1<<2);
-            PORTD &= ~(1<<2);
+            DDRC  |= (1<<2);
+            PORTC &= ~(1<<2);
             break;
         case 3:
-            DDRD  |= (1<<3);
-            PORTD &= ~(1<<3);
+            DDRC  |= (1<<3);
+            PORTC &= ~(1<<3);
             break;
         case 4:
-            DDRD  |= (1<<5);
-            PORTD &= ~(1<<5);
+            DDRC  |= (1<<4);
+            PORTC &= ~(1<<4);
             break;
-    }
+        case 5:
+            DDRC  |= (1<<5);
+            PORTC &= ~(1<<5);
+            break;
+        case 6:
+            DDRC  |= (1<<6);
+            PORTC &= ~(1<<6);
+            break;
+        case 7:
+            DDRC  |= (1<<7);
+            PORTC &= ~(1<<7);
+            break;
+    }   */
 }
